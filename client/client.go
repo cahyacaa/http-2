6235ae84589f804e896ae017a1eddaee4a66d67c
cahya-marine <cahya.nugroho@marinetechs.co>
2023-10-12 16:41:15 +0700

    change structure

--- client/client.go
+++ client/client.go
@@ -1,7 +1,16 @@
-package client
+package main
 
 import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
 	"sync"
+
+	"golang.org/x/net/http2"
 )
 
 func main() {
@@ -15,4 +24,66 @@ func main() {
 
 	}
 	wg.Wait()
+}
+
+func transport2() *http2.Transport {
+	return &http2.Transport{
+		TLSClientConfig:    tlsConfigClient(),
+		DisableCompression: true,
+		AllowHTTP:          false,
+	}
+}
+
+func tlsConfigClient() *tls.Config {
+	crt, err := os.ReadFile("./cert/public.crt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rootCAs := x509.NewCertPool()
+	rootCAs.AppendCertsFromPEM(crt)
+
+	return &tls.Config{
+		RootCAs:            rootCAs,
+		InsecureSkipVerify: true,
+		ServerName:         "localhost",
+	}
+}
+
+func Http2() {
+	client := &http.Client{Transport: transport2()}
+
+	res, err := client.Get("https://localhost:8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res.Body.Close()
+
+	fmt.Printf("Code: %d\n", res.StatusCode)
+	fmt.Printf("Body: %s\n", body)
+}
+
+func Http1() {
+	client := &http.Client{}
+
+	res, err := client.Get("https://localhost:8080/")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res.Body.Close()
+
+	fmt.Printf("Code: %d\n", res.StatusCode)
+	fmt.Printf("Body: %s\n", body)
 }
